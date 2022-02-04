8dbd6bef64a5111122d229c976de142a417eaf2e
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2022-02-04 14:14:28 -0500

    initial commit

---
+++ src/NameTag.js
@@ -0,0 +1,12 @@
+// import the react library to write JSX
+
+/* write an arrow function called "NameTag" that returns JSX
+The JSX returned should consist of:
+  - a div element with className attribute of "name-tag" and three nested children elements:
+    - an h3 element with className attribute of "title" and the inner text of "HELLO"
+    - a p element with className attribute of "subtitle" and the inner text of "my name is"
+    - a h2 element with className attribute of "name" and the inner text of your name
+*/
+
+/* export our functional component definition to be 
+imported then rendered within another module */
