1cb114c0d043698b4daf4ab90a2a42e4d711a060
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2022-02-04 14:32:12 -0500

    NameTag component function created and exported. React was imported.

--- src/NameTag.js
+++ src/NameTag.js
@@ -1,5 +1,5 @@
 // import the react library to write JSX
-
+import React from "react";
 /* write an arrow function called "NameTag" that returns JSX
 The JSX returned should consist of:
   - a div element with className attribute of "name-tag" and three nested children elements:
@@ -8,5 +8,14 @@ The JSX returned should consist of:
     - a h2 element with className attribute of "name" and the inner text of your name
 */
 
+const NameTag = () => (
+  <div className="name-tag">
+    <h3 className="title">HELLO</h3>
+    <p className="subtitle"> my name is</p>
+    <h2 className="name">Vanesa</h2>
+  </div>
+);
+
 /* export our functional component definition to be 
 imported then rendered within another module */
+export default NameTag;
